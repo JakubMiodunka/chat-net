57a3b0bc043fd42e525dc661007ff736b7215347
JakubMiodunka <miodunkajakub@gmail.com>
2025-01-26 20:58:02 +0100

    Adding handler for Simple Session Layer Protocol along with its tests.

---
+++ src/CommonUtilities/Protocols/IProtocol.cs
@@ -0,0 +1,8 @@
+﻿namespace CommonUtilities.Protocols;
+
+// TODO: Add doc-strings.
+public interface IProtocol
+{
+    byte[] ExtractPayload(IEnumerable<byte> packet);
+    byte[] PreparePacket(IEnumerable<byte> payload);
+}
