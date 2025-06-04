16198ce7b8910e748805923e0153690f39846c6c
JakubMiodunka <miodunkajakub@gmail.com>
2025-06-04 14:15:42 +0200

    Introducing RequestSerializer. It needs further development.

---
+++ src/UnitTests/Server/RequestSerializerTests.cs
@@ -0,0 +1,25 @@
+﻿using CommonUtilities.Models.Requests;
+using Server;
+
+
+namespace UnitTests.Server;
+
+// TODO: Finish implementation
+[Category("UnitTest")]
+[TestOf(typeof(RequestSerializer))]
+[Author("Jakub Miodunka")]
+public class RequestSerializerTests
+{
+    #region Test cases
+    [Test]
+    public void SerializationOfAuthenticateRequestIsTransparent()
+    {
+        var referenceRequest = new AuthenticateRequest(1, "asdadd");
+
+        byte[] serializedRequest = RequestSerializer.Serialize(referenceRequest);
+        AuthenticateRequest deserializedRequest = RequestSerializer.Deserialize<AuthenticateRequest>(serializedRequest);
+
+        Assert.That(deserializedRequest, Is.EqualTo(referenceRequest));
+    }
+    #endregion
+}
