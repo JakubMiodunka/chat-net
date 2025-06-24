2984430241db28272bb1a95530487412eb7c2bd5
JakubMiodunka <miodunkajakub@gmail.com>
2025-06-24 17:06:31 +0200

    Minor rearragning Server and UnitTests.Server namespaces to fit with current directory-tree convention. Preparing class for unit testing of ConnectionAuthenticator class.

---
+++ src/UnitTests/Server/Security/ConnectionAuthenticatorTests.cs
@@ -0,0 +1,12 @@
+﻿using Server.Security;
+
+
+namespace UnitTests.Server.Security;
+
+[Category("UnitTest")]
+[TestOf(typeof(ConnectionAuthenticator))]
+[Author("Jakub Miodunka")]
+public class ConnectionAuthenticatorTests
+{
+    // TODO: Prepare unit tests for ConnectionAuthenticator class.
+}
